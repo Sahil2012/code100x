e8df8ffc22a9e63b942f0eefee778a2640d055c2
unknown <sahilhsgupta7@gmail.com>
2024-04-28 02:07:30 +0530

    Completed purchase course functionality

--- week_04/03-mongo/routes/user.js
+++ week_04/03-mongo/routes/user.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
 const checkInputSign = require("../middleware/inputvalidation");
-const {User} = require("../db/index");
+const {User, Course} = require("../db/index");
 
 // User Routes
 router.post('/signup', checkInputSign,async (req, res) => {
@@ -19,17 +19,47 @@ router.post('/signup', checkInputSign,async (req, res) => {
     res.send("User Registered Sucessfully.")
 });
 
-router.get('/courses', (req, res) => {
+router.get('/courses',async (req, res) => {
     // Implement listing all courses logic
+    const allCourses = await Course.find();
+
+    res.json(allCourses);
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
+router.post('/courses/:courseId', userMiddleware,async (req, res) => {
     // Implement course purchase logic
+    const course = await Course.findOne({_id : req.params.courseId});
+
+    if(course) {
+        const user = await User.updateOne({username: req.headers.username},{$push:{purchasedCourses:course.id}});
+        console.log(user);
+        res.json({
+            "message":"Course purchased successfully"
+        });
+        return;
+    }
+    
+    res.json({
+        "message" : "Course unavailable"
+    })
+
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
-    // Implement fetching purchased courses logic
-    res.send('success');
+router.get('/purchasedCourses', userMiddleware,async (req, res) => {
+    
+    const user = await User.findOne({username : req.headers.username});
+    console.log(user.purchasedCourses);
+
+    const courses = await Course.find({_id : {$in:user.purchasedCourses}});
+
+    if(courses) {
+        res.json(courses);
+        return;
+    }
+
+    res.json({
+        "message" : "No courses yet"
+    })
 });
 
 module.exports = router
