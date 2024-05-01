cdb46393c1027016cf47fd328fbf99c1c9c0c2eb
unknown <sahilhsgupta7@gmail.com>
2024-05-01 23:08:33 +0530

    memo hook

---
+++ week_06/memo_wrapper_components/src/App.jsx
@@ -0,0 +1,43 @@
+import { memo } from "react";
+import { useState } from "react";
+
+const App = () => {
+
+  const [todos,setTodo] = useState([{
+    title : "t1",
+    description : "d1"
+  },{
+    title : "t2",
+    description : "d2"
+  },{
+    title : "t3",
+    description : "d3"
+  }]);
+
+  const handleAdd = () => {
+    setTodo([...todos,{
+      title: Math.random(),
+      description: Math.random()
+    }]);
+  }
+
+
+  return (<div>
+    <button onClick={handleAdd}>
+      Add Todo
+    </button>
+    <br />
+    {
+      todos.map(todo => <Todo title={todo.title} description={todo.description} />)
+    }
+  </div>);
+}
+
+const Todo = memo(({title,description}) => {
+  return (<div>
+    <h1>{title}</h1>
+    <h5>{description}</h5>
+  </div>);
+})
+
+export default App;
